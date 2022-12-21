1dea7affd3ac4c0c675349e7f8836df2ea7533ac
Daniar27 <daniarmiftah@gmail.com>
2022-12-21 18:29:27 +0700

    <add> Testimonial Component

---
+++ src/pages/Example.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import {InputDate} from 'elements/Forms/'
+
+export default class Example extends Component{
+    state={
+        value:{
+            startDate: new Date(),
+            endDate: new Date(),
+            key: "selection"
+        }
+    }
+
+    handleChange = e =>{
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+  render(){
+    return(
+        <div className='container'>
+            <div className='row align-items-center' style={{height : "100vh"}}>
+                <InputDate
+                 max={30}
+                 onChange={this.handleChange}
+                 name="value"
+                 value={this.state.value}
+                />
+            </div>
+        </div>
+    )
+  }
+}
